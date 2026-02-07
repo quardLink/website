0999a604e92210dc2d5b35e36753e9e2ac857bbc
Adarsh <adarshs8400@gmail.com>
2026-02-07 17:32:38 +0530

    switch base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // 👈 THIS IS THE KEY
+  base: "/", //
 });
